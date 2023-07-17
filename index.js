223bccf2b6ddbe7a9c0a0397534ef03bc45178c5
incognit0129 <55562089+incognit0129@users.noreply.github.com>
2023-07-16 23:25:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,31 +13,31 @@ for (let i = 0; i < buttons; i++) {
 function makeSound(buttonInnerhtml) {
   switch (buttonInnerhtml) {
     case "w":
-      var audio1 = new Audio("sounds/crash.mp3");
+      var audio1 = new Audio("crash.mp3");
       audio1.play();
       break;
     case "a":
-      var audio = new Audio("sounds/kick-bass.mp3");
+      var audio = new Audio("kick-bass.mp3");
       audio.play();
       break;
     case "s":
-      var audio = new Audio("sounds/snare.mp3");
+      var audio = new Audio("snare.mp3");
       audio.play();
       break;
     case "d":
-      var audio = new Audio("sounds/tom-1.mp3");
+      var audio = new Audio("tom-1.mp3");
       audio.play();
       break;
     case "j":
-      var audio = new Audio("sounds/tom-2.mp3");
+      var audio = new Audio("tom-2.mp3");
       audio.play();
       break;
     case "k":
-      var audio = new Audio("sounds/tom-3.mp3");
+      var audio = new Audio("tom-3.mp3");
       audio.play();
       break;
     case "l":
-      var audio = new Audio("sounds/tom-4.mp3");
+      var audio = new Audio("tom-4.mp3");
       audio.play();
       break;
     default:
